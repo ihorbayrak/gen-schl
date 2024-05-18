66ee38a6530753c55d5df6890981252d13415567
ihorbayrak <bayrak.igor563@gmail.com>
2024-05-18 18:59:12 +0300

    Create route and controller for /subscribe endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\API\RateController;
+use App\Http\Controllers\API\SubscribeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/rate', RateController::class);
+
+Route::post('/subscribe', SubscribeController::class);
